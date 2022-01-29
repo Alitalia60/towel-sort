757a31c2647ea49dc05f37533b949206e964df42
Andrew Litvinov <lita.study@bk.ru>
2022-01-29 10:12:21 +0300

    feat: test is successfull

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-
 // You should implement your task here.
+module.exports = function towelSort(matrix) {
+// function towelSort(matrix) {
+  if (matrix == undefined) {
+    return []
+  }
+  if (matrix.length ==0) {
+    return []
+  }
+  
+    let forward = false;
+    let sorted = [];
+    matrix.forEach((element) => {
+        forward = !forward;
+        if (forward) {
+            //no sort
+            element.forEach((symbol) => {
+                sorted.push(symbol);
+            });
+        } else {
+            //sort
+            for (let index = element.length-1; index >= 0; index--) {
+                sorted.push(element[index]);
+            }
+        }
+    });
+    return sorted;
+};
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// console.log(towelSort(matrix));
